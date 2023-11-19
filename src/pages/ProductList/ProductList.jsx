541ec64a0fd15f4bd232a240523190952df96125
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-11-19 23:16:15 +0700

    [update]

--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -15,36 +15,26 @@ import useProducts from './hooks/useProducts';
 
 export default function ProductList() {
 const { products } = useProducts();
-const renderProducts = () =>
-products &&
-products.length > 0 &&
-products.map((product) => (
+
+const renderProducts = () => products && products.length > 0 && products.map((product) => (
 <Card key={product.id} withBorder radius="md" className={classes.card}>
 <Card.Section className={classes.imageSection}>
-<Image
-src={product.images[0]}
-alt="Tesla Model S"
-w={'100%'}
-h={'100%'}
-fit="contain"
-/>
+<Image src={product.images[0]} alt="Tesla Model S" w={'100%'} h={'100%'} fit="contain"/>
 </Card.Section>
 <Group justify="space-between" mt="md">
 <div>
 <Text fw={500}>{product.title}</Text>
-<Text fz="xs" c="dimmed">
-Free recharge at any station
-</Text>
+<Text fz="xs" c="dimmed">Free recharge at any station</Text>
 </div>
 <Badge variant="outline">25% off</Badge>
 </Group>
 <Card.Section className={classes.section}>
 <Group gap={30} justify="space-between" px={15}>
 <div>
-<Text fz="xl" fw={700} style={{ lineHeight: 1
-}}>
-{product.price}
-</Text></div>
+<Text fz="xl" fw={700} style={{ lineHeight: 1}}>
+     {product.price}
+</Text>
+</div>
 <Link to={`/products/${product.id}`}>
 <Button radius="xl" style={{ flex: 1 }}>
 Beli
@@ -54,6 +44,7 @@ Beli
 </Card.Section>
 </Card>
 ));
+
 return (
 <Container py="xl">
 <ProductSearch />
